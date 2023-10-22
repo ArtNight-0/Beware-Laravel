da3fa36a545d2e999db1a8d8abea962d49a47988
Artnight-0 <arohendi90@gmail.com>
2023-10-22 13:56:57 +0700

    navbar

---
+++ resources/views/partialas/navbar/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="{{asset('assets/img/Logo-unnormalii.png')}}" alt="Logo-Unnormali" width="200"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto ">
+          <li class="mx-2 nav-item">
+            <h5><a class="text-dark nav-link active" aria-current="page" href="#">Our Design</a></h5>
+          </li>
+          <li class="mx-2 nav-item">
+            <h5><a class="text-dark nav-link" href="#">About</a></h5>
+          </li>
+          <li class="mx-2 nav-item">
+            <h5><a class="text-dark nav-link " href="#">Contact Us</a></h5>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
